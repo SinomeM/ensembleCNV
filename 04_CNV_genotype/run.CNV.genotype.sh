eac4be51ae3ec1d229b8adbfed69590538038c3c
Simone Montalbano <simone.montalbano@protonmail.com>
2020-07-01 10:52:33 +0200

    created minimal versione of EnsembleCNV calling pipeline

--- 04_CNV_genotype/run.CNV.genotype.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-Rscript CNV.genotype.one.chr.one.batch.R \
-            --type $TYPE \ 
-            --datapath $DTPATH \ 
-            --resultpath $RESPATH \ 
-            --matrixpath $MATPATH \ 
-            --sourcefile $SOURCE
--- 05_boundary_refinement/run.boundary.refinement.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-Rscript ${WKDIR}/05_boundary_refinement/CNVR.boundary.refinement.R \ 
-            --datapath $DTPATH \ 
-            --resultpath $RESPATH \ 
-            --matrixpath $MATPATH \ 
-            --rcppfile $RCPPFILE \ 
-            --centromere $CENTROMERE 
